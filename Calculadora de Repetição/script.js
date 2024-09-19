2cabd0691ca95b9980ca3ea77deaed598dade404
WilliamRodriguesDS <williamrodriguesdeoliveira22@gmail.com>
2024-09-19 20:04:44 -0300

    Sla

--- Calculadora de Repetição/script.js
+++ Calculadora de Repetição/script.js
@@ -1,44 +1,45 @@
 function iniciarCalculoeCalcular(){
     while(true){
-        let menu = prompt("Digite 1 para adição ou 2 para substração ou 3 para multiplicação ou 4 para divisão ou 5 para sair");
+        let operacao = parseFloat(prompt("Digite um número para:\n" + "1 - adição \n"+ "2 - substração\n" + "3 - multiplicação\n" + "4 - divisão\n" + "5 - sair\n"));
 
-        let menuValor = parseInt(menu);
+        let resultado;
 
-        if(menuValor === 5){
-            alert("Programa terminado por sair!");
-            break;
+        if(operacao !== 1 && operacao !== 2 && operacao !== 3 && operacao !== 4 && operacao !== 5){
+            alert("Operador não identificado, tente novamente")
         }
-
-        if(menu === null){
-            alert("Programa terminado por cancelar!");
-            break;
-        }
-
-        let n1 = parseFloat(prompt("Digite um número"));
-        let n2 = parseFloat(prompt("Digite outro número"));
-
-        switch(menuValor){
-            case 1:
-                alert(n1 + n2);
+        else{
+            if(operacao === 5){
+                alert("Programa encerrado");
                 break;
-            case 2:
-                alert(n1 - n2);
-                break;
-            case 3:
-                alert(n1 * n2);
-                break;
-            case 4:
-                if(n1 === 0 || n2 === 0){
-                    alert("Erro, tente novamente!");
-                }
-                else{
-                     alert(n1 / n2);
-                }
-            default:
-                    alert("Operação não idenficado, tente novamente");
-
             }
+            else{
+                let n1 = parseFloat(prompt("Digite um número"));
+                let n2 = parseFloat(prompt("Digite outro número"));
 
-
+            switch(operacao){
+                case 1:
+                    resultado = "O resultado da soma = " + (n1 + n2);
+                    alert(resultado);
+                    break;
+                case 2:
+                    resultado = "O resultado da substração = " + (n1 - n2);
+                    alert(resultado);
+                    break;
+                case 3:
+                    resultado = "O resultado da multiplicação = " + (n1 - n2);
+                    alert(n1 * n2);
+                    break;
+                case 4:
+                    if(n1 === 0 || n2 === 0){
+                        alert("Erro! impossível dividir com número 0");
+                    }
+                    else{
+                        resultado = "O resultado da divisão = " + (n1 - n2);
+                        alert(resultado);
+                    }
+                    break;
+            }
+            }
     }
 }
+    }
